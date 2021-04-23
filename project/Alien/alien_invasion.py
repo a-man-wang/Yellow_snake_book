21323d6e0110b09a4cb6ad76c143aeaec97f5f0e
lao wang <wangjiangtao@gmail.com>
2021-04-23 15:00:19 +0800

    12.3.3 创建设置类

--- project/Alien/alien_invasion.py
+++ project/Alien/alien_invasion.py
@@ -1,16 +1,19 @@
 import sys
 import pygame
 
+from settings import Settings
+
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
     def __init__(self):
         """初始化游戏并创建游戏资源"""
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_heigh))
         pygame.display.set_caption("Aline Invasion")
         # 设置背景色
-        self.bg_color = (105, 139, 105)
+        # self.bg_color = (105, 139, 105)
 
     def run_game(self):
         """开始游戏主循环"""
@@ -20,7 +23,7 @@ class AlienInvasion:
                 if envent.type == pygame.QUIT:
                     sys.exit()
             # 每次循环重绘屏幕
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             # 让最近绘制的屏幕可见
             pygame.display.flip()
 
---
+++ project/Alien/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """初始化游戏设置"""
+    def __init__(self):
+        """初始化游戏设置"""
+        # 屏幕设置
+        self.screen_width = 1200
+        self.screen_heigh = 800
+        self.bg_color = (105, 139, 105)
