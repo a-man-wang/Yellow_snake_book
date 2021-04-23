c75982b396639f609c978959d019fcf5bb94026e
lao wang <wangjiangtao@gmail.com>
2021-04-23 14:33:27 +0800

    12.3.2 设置背景色

--- project/Alien/alien_invasion.py
+++ project/Alien/alien_invasion.py
@@ -7,8 +7,10 @@ class AlienInvasion:
     def __init__(self):
         """初始化游戏并创建游戏资源"""
         pygame.init()
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Aline Invasion")
+        # 设置背景色
+        self.bg_color = (105, 139, 105)
 
     def run_game(self):
         """开始游戏主循环"""
@@ -17,6 +19,8 @@ class AlienInvasion:
             for envent in pygame.event.get():
                 if envent.type == pygame.QUIT:
                     sys.exit()
+            # 每次循环重绘屏幕
+            self.screen.fill(self.bg_color)
             # 让最近绘制的屏幕可见
             pygame.display.flip()
 
