6978accc485dcac079a38e868de1bc59a2d3deb6
lao wang <wangjiangtao@gmail.com>
2021-04-25 17:58:26 +0800

    习题12.4

--- exercises/十二/#12.4.py
+++ exercises/十二/#12.4.py
@@ -1,26 +1,22 @@
-import sys
+"""
+
+练习12-4：火箭 编写一个游戏，它在屏幕中央显示一个火箭，而玩家可使用四个方向键上下左右移动火箭。请务必确保火箭不会移到屏幕外面。
+
+"""
+
 import pygame
+import sys
+from ico import Ico
 
-from settings import Settings
-from ship import Ship
 
+class BlueScreen():
 
-class AlienInvasion:
-    """管理游戏资源和行为的类"""
     def __init__(self):
-        """初始化游戏并创建游戏资源"""
         pygame.init()
-        self.settings = Settings()
-        # 窗口设置
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_heigh))
-        # 全屏设置
-        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-        # self.settings.screen_width = self.screen.get_rect().width
-        # self.settings.screen_heigh = self.screen.get_rect().height
-        pygame.display.set_caption("Aline Invasion")
-        self.ship = Ship(self)
-        # 设置背景色
-        # self.bg_color = (105, 139, 105)
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("bilibili")
+        self.bg_color = (230, 230, 230)
+        self.ico = Ico(self)
 
     def _check_events(self):
         # 监听游戏和键盘事件
@@ -32,27 +28,35 @@ class AlienInvasion:
             elif envent.type == pygame.KEYUP:
                 self._check_keyup_events(envent)
 
-    def _check_keydown_events(self,envent):
+    def _check_keydown_events(self, envent):
         """响应按下按键"""
         if envent.key == pygame.K_RIGHT:
             # 向右移动飞船
-            self.ship.moving_right = True
+            self.ico.moving_right = True
         elif envent.key == pygame.K_LEFT:
-            self.ship.moving_left = True
+            self.ico.moving_left = True
+        elif envent.key == pygame.K_UP:
+            self.ico.moving_top = True
+        elif envent.key == pygame.K_DOWN:
+            self.ico.moving_bottom = True
         elif envent.key == pygame.K_q:
             sys.exit()
-            
+
     def _check_keyup_events(self, envent):
         """响应弹起按键"""
         if envent.key == pygame.K_RIGHT:
-            self.ship.moving_right = False
+            self.ico.moving_right = False
         elif envent.key == pygame.K_LEFT:
-            self.ship.moving_left = False
+            self.ico.moving_left = False
+        elif envent.key == pygame.K_UP:
+            self.ico.moving_top = False
+        elif envent.key == pygame.K_DOWN:
+            self.ico.moving_bottom = False
 
     def _update_screen(self):
         # 每次循环重绘屏幕
-        self.screen.fill(self.settings.bg_color)
-        self.ship.blitme()
+        self.screen.fill(self.bg_color)
+        self.ico.blitme()
         # 让最近绘制的屏幕可见
         pygame.display.flip()
 
@@ -60,10 +64,10 @@ class AlienInvasion:
         """开始游戏主循环"""
         while True:
             self._check_events()
-            self.ship.update()
+            self.ico.update()
             self._update_screen()
 
 
 if __name__ == '__main__':
-    ai = AlienInvasion()
-    ai.run_game()
+    blue = BlueScreen()
+    blue.run_game()
--- exercises/十二/ico.py
+++ exercises/十二/ico.py
@@ -3,16 +3,34 @@ import pygame
 
 class Ico:
     """kklt类"""
-    def __init__(self,my_game):
-        """初始化角色并设置初始位置"""
-        self.screen = my_game.screen
-        self.screen_rect = my_game.screen.get_rect()
-        # 加载图形极其外接矩形
-        self.image = pygame.image.load("images/ico.bmp")
+    def __init__(self,ai_game):
+        """初始化飞船位置并设置其初始位置"""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        # 加载飞船图像并获得其外接矩形
+        self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
-        #新角色设置到 屏幕中央底部
+        # 对于每一艘新飞船，都将其放在屏幕底部中央
         self.rect.midbottom = self.screen_rect.midbottom
+        # 飞船属性x可以存小数
+        self.x = float(self.rect.x)
+        # 移动标志
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_top = False
+        self.moving_bottom = False
+
+    def update(self):
+        """根据moving_right标志符移动飞船"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.x += 1
+        if self.moving_left and self.rect.left > 0:
+            self.rect.x -= 1
+        if self.moving_bottom and self.rect.bottom < self.screen_rect.bottom:
+            self.rect.y += 1
+        if self.moving_top and self.rect.top > 0:
+            self.rect.y -= 1
 
     def blitme(self):
-        """指定位置绘制飞机"""
-        self.screen.blit(self.image,self.rect)
+        """在指定位置绘制飞机"""
+        self.screen.blit(self.image, self.rect)
